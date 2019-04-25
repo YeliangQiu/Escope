ff2e1a44a716b0d504340e420af6b8aaa45a7a78
仇烨亮 <qiuyeliang@MacBook-Pro.local>
2019-04-25 15:45:06 +0800

    添加贪心算法

---
+++ src/eScope/Greed.java
@@ -0,0 +1,9 @@
+package eScope;
+
+/**
+ * @author : qiuyeliang
+ * create at:  2019/4/25  10:08
+ * @description: 贪心算法求解
+ */
+public class Greed {
+}
---
+++ src/eScope/compara/ComparatorPeakEE.java
@@ -0,0 +1,9 @@
+package eScope;
+
+/**
+ * @author : qiuyeliang
+ * create at:  2019/4/25  11:12
+ * @description: 比较器
+ */
+public class ComparatorPeakEE {
+}
